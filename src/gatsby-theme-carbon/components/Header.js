6b046147822170b1c92b63e9b7789fc17e730ec8
Alex <atatner@gmail.com>
2019-09-05 14:52:13 +0100

    Header
    Update header styles

--- src/gatsby-theme-carbon/components/Header.js
+++ src/gatsby-theme-carbon/components/Header.js
@@ -3,8 +3,26 @@ import Header from 'gatsby-theme-carbon/src/components/Header';
 
 const CustomHeader = props => (
   <Header {...props}>
-    Hai
+  <div
+  style={divStyle}
+  >
+
+  <img 
+    alt={'Logo'} 
+    src={require('../../images/roundel.svg')} 
+    style={logoStyle}
+    />
+  
+  </div>
   </Header>
 );
+
+var divStyle = {
+  marginTop: "28px",
+};
+
+var logoStyle = {
+  minHeight: "32px",
+};
 
 export default CustomHeader;
